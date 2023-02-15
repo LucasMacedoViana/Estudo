5db02ebb4067597c15e6a7f0396b6a26e243ba4e
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-02-15 19:27:23 -0300

    atualizando o projeto

---
+++ Flutter/projeto_refeicoes/lib/screens/categories_meals_creens.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_refeicoes/models/meal.dart';
+import '../models/category.dart';
+import '../data/dummy_data.dart';
+import '../components/meal_item.dart';
+
+class CategoriesMealsScreens extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: (ctx, index) {
+            return MealItem(categoryMeals[index]);
+          }),
+    );
+  }
+}
