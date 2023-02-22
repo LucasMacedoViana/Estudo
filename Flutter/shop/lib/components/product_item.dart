789dbd4a434283959d708c8137c9eab8152e23da
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-02-22 09:13:34 -0300

    finalizando o modulo 9

---
+++ Flutter/shop/lib/components/product_item.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/utils/app_routes.dart';
+
+import '../models/product_list.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem(this.product, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      title: Text(product.name),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(AppRoutes.PRODUCT_FORM, arguments: product);
+              },
+              icon: Icon(Icons.edit),
+              color: Theme
+                  .of(context)
+                  .primaryColor,
+            ),
+            IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) =>
+                      AlertDialog(
+                        title: Text('Excluir produto?'),
+                        content: Text('Tem certeza?'),
+                        actions: [
+                          TextButton(onPressed: () =>
+                              Navigator.of(context)
+                                  .pop(), child: Text('Não'),),
+                          TextButton(onPressed: () {
+                            Provider.of<ProductList>(context, listen: false)
+                                .removeProduct(product);
+                            Navigator.of(context).pop();
+                          }, child: Text('Sim'),),
+                        ],
+                      ),
+                );
+
+              },
+              icon: Icon(Icons.delete),
+              color: Theme
+                  .of(context)
+                  .errorColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Flutter/shop/lib/main.dart
+++ Flutter/shop/lib/main.dart
@@ -5,6 +5,8 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/product_form_page.dart';
+import 'package:shop/pages/product_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -44,6 +46,8 @@ class MyApp extends StatelessWidget {
           AppRoutes.HOME: (cts) => ProductsOverviewPage(),
           AppRoutes.CART: (cts) => CartPage(),
           AppRoutes.ORDERS: (cts) => OrdersPage(),
+          AppRoutes.PRODUCTS: (cts) => ProductsPage(),
+          AppRoutes.PRODUCT_FORM: (cts) => ProductFormPage(),
         },
       ),
     );
