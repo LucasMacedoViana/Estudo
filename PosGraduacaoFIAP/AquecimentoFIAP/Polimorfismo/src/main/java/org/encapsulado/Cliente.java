1209ced13738f1f81819582be8e2bc73535ba647
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-03-11 20:53:52 -0300

    iniciando polimorfismo

---
+++ PosGraduacaoFIAP/AquecimentoFIAP/Polimorfismo/src/main/java/org/encapsulado/Cliente.java
@@ -0,0 +1,34 @@
+package org.encapsulado;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String profissao;
+
+
+}
---
+++ PosGraduacaoFIAP/AquecimentoFIAP/Polimorfismo/src/main/java/org/encapsulado/TestaGetESet.java
@@ -0,0 +1,25 @@
+package org.encapsulado;
+
+public class TestaGetESet {
+    public static void main(String[] args) {
+        Conta conta = new Conta(1337,1234);
+        //conta.numero = 1336; não funciona
+        conta.setNumero(1337);
+        System.out.println(conta.getNumero());
+        Cliente lucas = new Cliente();
+        //conta.titular = "Lucas";
+        conta.setTitular(lucas);
+        lucas.setNome("Lucas Macedo");
+        System.out.println(lucas.getNome());
+        System.out.println(conta.getTitular().getNome());
+        conta.getTitular().setProfissao("Programador");
+        //em duas linhas
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("Programador");
+
+        System.out.println(titularDaConta);
+        System.out.println(lucas);
+        System.out.println(conta.getTitular());
+
+    }
+}
