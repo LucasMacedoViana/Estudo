2bd7b7e9fd993554f30eeadcaf8064fbf66570bb
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-04-02 11:21:07 -0300

    finaliznado a auth page

---
+++ Flutter/chat/lib/pages/auth_page.dart
@@ -0,0 +1,46 @@
+import 'package:chat/components/auth_form.dart';
+import 'package:chat/models/auth_form_data.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+    print(formData.name);
+    print(formData.email);
+    print(formData.password);
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading) Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.5),
+            ),
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
