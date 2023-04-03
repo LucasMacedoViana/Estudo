07e009c76f3918054c1e256b3a7565d3e294be80
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-04-03 13:59:09 -0300

    atualiznado

--- Flutter/chat/lib/pages/auth_page.dart
+++ Flutter/chat/lib/pages/auth_page.dart
@@ -1,5 +1,5 @@
 import 'package:chat/components/auth_form.dart';
-import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/core/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -12,12 +12,23 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
-    print(formData.name);
-    print(formData.email);
-    print(formData.password);
-    setState(() => _isLoading = false);
+  Future <void> _handleSubmit(AuthFormData formData) async {
+    try{
+      setState(() {
+        _isLoading = true;
+      });
+      if(formData.isLogin){
+        //login
+      }else {
+        //Signup
+      }
+    }catch(erro){
+      // tratar erro
+    } finally{
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
