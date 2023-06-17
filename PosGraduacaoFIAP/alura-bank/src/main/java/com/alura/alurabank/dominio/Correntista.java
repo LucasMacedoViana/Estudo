263c3e9e11b0f085da64f546da89b0b739a36c47
Lucas Macedo Viana <lucasmacedo9@hotmail.com>
2023-06-17 10:21:49 -0300

    Finalizando modulo 2.3

--- PosGraduacaoFIAP/alura-bank/src/main/java/com/alura/alurabank/dominio/Correntista.java
+++ PosGraduacaoFIAP/alura-bank/src/main/java/com/alura/alurabank/dominio/Correntista.java
@@ -1,14 +1,18 @@
 package com.alura.alurabank.dominio;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
 public class Correntista {
+    @JsonProperty
     private String nome;
+    @JsonProperty
     private String cpf;
 
-    public String getCpf() {
-        return cpf;
-    }
-    public String getNome() {
-        return nome;
+    private LocalDate dataDeEntrada = LocalDate.now();
+    public LocalDate getDataDeEntrada() {
+        return dataDeEntrada;
     }
 
 }
